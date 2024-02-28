fbcf1be7cfac29a254aff59d4f5b6b0e6461bb30
Nhutranh <nhutranh008@gmail.com>
2024-02-28 19:20:01 +0700

    exam-client

---
+++ exam_client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+//import Login from './Authentication/Login/Login';
+import DashBoard from './DashBoard/DashBoard';
+
+function App() {
+  return (
+    <DashBoard></DashBoard>
+    
+  );
+}
+
+export default App;
