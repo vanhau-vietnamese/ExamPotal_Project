99035f2fb1636d7c4d8fd41b2e80223fa169f5be
tfengaka <nthoa2.lhp.nh@gmail.com>
2024-02-28 20:16:21 +0700

    split branch frontend

--- exam_server/src/main/java/com/exam/repo/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exam.repo;
-
-import com.exam.model.exam.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
