5008986e66d88d1dae28ec7545563834dab9e5fc
tfenaka <nthoa2.lhp.nh@gmail.com>
2024-03-03 10:26:36 +0700

    update db schema

--- ExamPotal_Database.sql
+++ ExamPotal_Database.sql
@@ -1,86 +1,130 @@
 create database exam_project;
 use exam_project;
--- ROLE TABLE --
-CREATE TABLE `role` (
-	`roleId` BIGINT AUTO_INCREMENT PRIMARY KEY,
-	`roleName` VARCHAR(50) NOT NULL
-);
-INSERT INTO `exam_project`.`role` (`roleId`, `roleName`) VALUES ('1', 'ROLE_ADMIN');
-INSERT INTO `exam_project`.`role` (`roleId`, `roleName`) VALUES ('2', 'ROLE_STUDENT');
-INSERT INTO `exam_project`.`role` (`roleId`, `roleName`) VALUES ('3', 'ROLE_TEACHER');
 
--- USER TABLE --
-CREATE TABLE `user` (
-	`userId` BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(50),
-	`username` VARCHAR(50) NOT NULL,
-	`password` VARCHAR(100) NOT NULL,
-	`email` VARCHAR(100) NOT NULL,
-    `roleId` BIGINT,
-    FOREIGN KEY (`roleId`) REFERENCES `role`(`roleId`)
-);
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- CATEGORY ROLE --
-CREATE TABLE `Category` (
-  `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-  `title` VARCHAR(255) NOT NULL,
-  `description` TEXT,
-  `createBy` BIGINT,
-  FOREIGN KEY (`createBy`) REFERENCES `user`(`userId`)
-);
+-- ----------------------------
+-- Table structure for answers
+-- ----------------------------
+DROP TABLE IF EXISTS `answers`;
+CREATE TABLE `answers`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `media` json NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `isCorrect` tinyint(1) NOT NULL DEFAULT 0,
+  `status` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `questionId` bigint NULL DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `questionId`(`questionId` ASC) USING BTREE,
+  CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`questionId`) REFERENCES `questions` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
--- QUIZ TABLE --
-CREATE TABLE `Quiz` (
-  `quizId` BIGINT AUTO_INCREMENT PRIMARY KEY,
-  `title` VARCHAR(255) NOT NULL,
-  `description` TEXT,
-  `maxMarks` INT NOT NULL,
-  `numberOfQuestions` INT NOT NULL,
-  `active` BOOLEAN DEFAULT FALSE,
-  `categoryId` BIGINT,
-  FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`)
-);
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `createBy` bigint NULL DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `createBy`(`createBy` ASC) USING BTREE,
+  CONSTRAINT `category_ibfk_1` FOREIGN KEY (`createBy`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
--- HISTORY TABLE --
-CREATE TABLE `user_quiz_history` (
-  `user_id` BIGINT,
-  `quiz_id` BIGINT,
-  PRIMARY KEY (user_id, quiz_id),
-  FOREIGN KEY (user_id) REFERENCES `User`(userId),
-  FOREIGN KEY (quiz_id) REFERENCES `Quiz`(quizId)
-);
+-- ----------------------------
+-- Table structure for question_types
+-- ----------------------------
+DROP TABLE IF EXISTS `question_types`;
+CREATE TABLE `question_types`  (
+  `alias` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `displayName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`alias`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
--- QUESTION TYPE TABLE --
-CREATE TABLE `Question_Type` (
-  `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-  `displayName` VARCHAR(50) NOT NULL	
-);
-INSERT INTO `exam_project`.`question_type` (`id`, `displayName`) VALUES ('1', 'Trắc nghiệm');
-INSERT INTO `exam_project`.`question_type` (`id`, `displayName`) VALUES ('2', 'Tự luận');
+-- ----------------------------
+-- Table structure for questions
+-- ----------------------------
+DROP TABLE IF EXISTS `questions`;
+CREATE TABLE `questions`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `media` json NULL,
+  `questionType` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `status` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `questionType`(`questionType` ASC) USING BTREE,
+  CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`questionType`) REFERENCES `question_types` (`alias`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for quiz_questions
+-- ----------------------------
+DROP TABLE IF EXISTS `quiz_questions`;
+CREATE TABLE `quiz_questions`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `quizId` bigint NULL DEFAULT NULL,
+  `questionId` bigint NULL DEFAULT NULL,
+  `status` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `quizId`(`quizId` ASC) USING BTREE,
+  INDEX `questionId`(`questionId` ASC) USING BTREE,
+  CONSTRAINT `quiz_questions_ibfk_1` FOREIGN KEY (`quizId`) REFERENCES `quizzes` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `quiz_questions_ibfk_2` FOREIGN KEY (`questionId`) REFERENCES `questions` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
--- QUESTION TABLE --
-CREATE TABLE `Question` (
-  `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-  `content` BLOB NULL, -- tk này dùng cho câu trả lời dạng hình ảnh or âm thanh
-  `text_content` TEXT NULL, -- tk này thì dùng cho dạng văn bản
-  `type_id` BIGINT,
-  `quiz_id` BIGINT,
-  FOREIGN KEY (`type_id`) REFERENCES `Question_Type`(`id`),
-  FOREIGN KEY (`quiz_id`) REFERENCES `Quiz`(`quizId`)
-);
+-- ----------------------------
+-- Table structure for quizzes
+-- ----------------------------
+DROP TABLE IF EXISTS `quizzes`;
+CREATE TABLE `quizzes`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `title` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `maxMarks` int NOT NULL,
+  `numberOfQuestions` int NOT NULL,
+  `status` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `categoryId` bigint NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `categoryId`(`categoryId` ASC) USING BTREE,
+  CONSTRAINT `quizzes_ibfk_1` FOREIGN KEY (`categoryId`) REFERENCES `category` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
--- ANSWER TABLE --
-CREATE TABLE `Answer` (
-  `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-  `content` BLOB NOT NULL, -- tk này dùng cho câu trả lời dạng hình ảnh or âm thanh
-  `text_content` TEXT NOT NULL, -- tk này thì dùng cho dạng văn bản
-  `isCorrect` BOOLEAN NOT NULL,
-  `question_id` BIGINT,
-  FOREIGN KEY (question_id) REFERENCES Question(id)
-);
+-- ----------------------------
+-- Table structure for user_quiz_results
+-- ----------------------------
+DROP TABLE IF EXISTS `user_quiz_results`;
+CREATE TABLE `user_quiz_results`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `userId` bigint NULL DEFAULT NULL,
+  `quizId` bigint NULL DEFAULT NULL,
+  `marks` int NULL DEFAULT NULL,
+  `capture` json NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `userId`(`userId` ASC) USING BTREE,
+  INDEX `quizId`(`quizId` ASC) USING BTREE,
+  CONSTRAINT `user_quiz_results_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `user_quiz_results_ibfk_2` FOREIGN KEY (`quizId`) REFERENCES `quizzes` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `fullName` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `firebaseId` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `role` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
-
-
-
+SET FOREIGN_KEY_CHECKS = 1;
