d932fc0285fa906df8431f1a218d3bb3b3ee0c26
tfengaka <nthoa2.lhp.nh@gmail.com>
2024-03-24 14:20:47 +0700

    fix z-index

--- exam_student/src/layouts/components/Header.jsx
+++ exam_student/src/layouts/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
   const [openMenu, setOpenMenu] = useState(false);
 
   return (
-    <div className="fixed w-[calc(100%-280px)] h-[64px] flex items-center py-1 backdrop-blur-[6px] ml-[280px] z-0 bg-[#e4e6e8] bg-opacity-15">
+    <div className="fixed w-[calc(100%-280px)] h-[64px] flex items-center py-1 backdrop-blur-[6px] ml-[280px] bg-[#e4e6e8] bg-opacity-15 z-[9999]">
       <div className="flex items-center justify-between w-full px-4">
         <div className="flex items-center gap-4 text-icon">
           <Icons.Search />
