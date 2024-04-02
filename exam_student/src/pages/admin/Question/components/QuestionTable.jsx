79e05f62086c00cc8f3af62350eeee53cae64f7c
Nhutranh <nhutranh008@gmail.com>
2024-04-02 10:23:38 +0700

    update createCaterogy

--- exam_student/src/pages/admin/Question/components/QuestionTable.jsx
+++ exam_student/src/pages/admin/Question/components/QuestionTable.jsx
@@ -11,12 +11,12 @@ function QuestionTable() {
       <table className="block w-full text-sm text-left rtl:text-right border-collapse">
         <thead className="text-[#3b3e66] uppercase text-xs block w-full">
           <tr className="bg-[#d1d2de] rounded-se w-full flex items-center">
-            <th className="p-3 w-[10%]">Mã số</th>
-            <th className="p-3 flex-auto">Nội dung câu hỏi</th>
-            <th className="p-3 flex-shrink-0 min-w-[250px]">Loại câu hỏi</th>
-            <th className="p-3 flex-shrink-0 min-w-[300px]">Danh mục</th>
-            <th className="p-3 flex-shrink-0 min-w-[180px]">Ngày tạo</th>
-            <th className="p-3 flex-shrink-0 min-w-[180px]" align="center">
+            <th className="p-3 w-[10%] ">Mã số</th>
+            <th className="p-3 flex-auto min-w-[200px]">Nội dung câu hỏi</th>
+            <th className="p-3 flex-shrink-0 min-w-[200px]">Loại câu hỏi</th>
+            <th className="p-3 flex-shrink-0 min-w-[150px]">Danh mục</th>
+            <th className="p-3 flex-shrink-0 min-w-[100px]">Ngày tạo</th>
+            <th className="p-3 flex-shrink-0 min-w-[150px]" align="center">
               Hành động
             </th>
           </tr>
@@ -28,18 +28,20 @@ function QuestionTable() {
                 key={question.id}
                 className="flex items-center border-b border-[#d1d2de] transition-all hover:bg-[#d1d2de] hover:bg-opacity-30 h-[45px] font-semibold text-[#3b3e66]"
               >
-                <td className="p-3 w-[10%]">{question.id}</td>
-                <td className="p-3 flex-auto text-nowrap text-ellipsis overflow-hidden">
+                <td className="p-3 w-[50px]">{question.id}</td>
+                <td className="p-3 flex-auto min-w-[200px] text-nowrap text-ellipsis overflow-hidden">
                   <EditorViewer content={question.content} />
                 </td>
-                <td className="p-3 flex-shrink-0 w-[250px]">
+                <td className="p-3 flex-shrink-0 min-w-[180px]">
                   {question.questionType?.displayName}
                 </td>
-                <td className="p-3 flex-shrink-0 w-[300px]">{question.category?.title || '--'}</td>
-                <td className="p-3 overflow-hidden flex-shrink-0 w-[200px]">
+                <td className="p-3 flex-shrink-0 min-w-[100px]">
+                  {question.category?.title || '--'}
+                </td>
+                <td className="p-3 overflow-hidden flex-shrink-0 min-w-[100px]">
                   {moment(question.createdAt).format('HH:mm, DD/MM/YYYY')}
                 </td>
-                <td className="p-3 flex-shrink-0">
+                <td className="p-3 flex-shrink-0 min-w-[150px]">
                   <div className=" flex items-center justify-center gap-x-2">
                     <Button className="text-xs rounded px-2 py-1 text-orange-500 hover:bg-orange-200 hover:bg-opacity-40">
                       <Icons.Eye />
